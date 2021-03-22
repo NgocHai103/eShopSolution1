d6cfa6bce4bf57f073591376da0d73af06bf479d
NgocHai <lengochai103@gmail.com>
2021-03-22 08:07:55 -0700

    Add Entities

---
+++ eShopSolution.Data/Entities/Category.cs
@@ -0,0 +1,19 @@
+﻿using eShopSolution.Data.Enums;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace eShopSolution.Data.Entities
+{
+    public class Category
+    {
+        public int Id     {set;get;}
+        public int Name   {set;get;}
+        public int SortOrder{set;get;}
+        public int IsShowOnHome{set;get;}
+        public int? ParentId{set;get;}
+        public Status Status {set;get;}
+        public int SeoDescription{set;get;}
+        public int SeoTitl { set; get; }
+    }
+}
