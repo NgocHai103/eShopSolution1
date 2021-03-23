2e6b60e6cdc02f3f50aee971887a6036b3af179a
ngochai <lengochai103@gmail.com>
2021-03-23 15:41:30 +0700

    identity database

--- eShopSolution.Data/EF/EShopDBContext.cs
+++ eShopSolution.Data/EF/EShopDBContext.cs
@@ -2,6 +2,7 @@
 using eShopSolution.Data.Entities;
 using eShopSolution.Data.Extensions;
 using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
@@ -9,7 +10,7 @@ using System.Text;
 
 namespace eShopSolution.Data.EF
 {
-    public class EShopDBContext:DbContext
+    public class EShopDBContext: IdentityDbContext<AppUser,AppRole,Guid>
     {
         public EShopDBContext(DbContextOptions options) : base(options)
         {
