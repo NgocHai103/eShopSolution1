51cf5c536bb1acd29290cc98b38dba752ebee60b
NgocHai <lengochai103@gmail.com>
2021-03-24 02:25:28 -0700

    Create/Update/Delete Products

---
+++ eShopSolution.Application/Catalog/Products/IManageProductSevice.cs
@@ -0,0 +1,22 @@
+﻿using eShopSolution.Application.Catalog.Products.Dtos;
+using eShopSolution.Application.Catalog.Products.Dtos.Manage;
+using eShopSolution.Application.Dtos;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace eShopSolution.Application.Catalog.Products
+{
+    public interface IManagerProductSevice
+    {
+       public Task<int> Create(ProductCreateRequest request);
+        public Task<int> Update(ProductUpdateRequest request);
+        public Task<int> Delete(int productId);
+
+        public Task<bool> UpdatePrice(int productId,decimal newPrice);
+        public Task AddViewcount(int productId);
+        public Task<bool> UpdateStock(int productId,int addedQuantity);
+        public Task<PageResult<ProductViewModel>> GetAllPaging(GetProductPagingRequest request);
+    }
+}
---
+++ eShopSolution.Application/Catalog/Products/ManageProductService.cs
@@ -0,0 +1,142 @@
+﻿using eShopSolution.Application.Catalog.Products.Dtos;
+using eShopSolution.Application.Catalog.Products.Dtos.Manage;
+using eShopSolution.Application.Dtos;
+using eShopSolution.Data.EF;
+using eShopSolution.Data.Entities;
+using eShopSolution.Utilities.Exceptions;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+using System.Linq;
+using Microsoft.EntityFrameworkCore;
+
+namespace eShopSolution.Application.Catalog.Products
+{
+    public class ManageProductService : IManagerProductSevice
+    {
+        private readonly EShopDBContext _context;
+        public ManageProductService(EShopDBContext context)
+        {
+            _context = context;
+        }
+        public async Task AddViewcount(int productId)
+        {
+            var product = await _context.Products.FindAsync(productId);
+            product.ViewCount += 1;
+            await _context.SaveChangesAsync();
+        }
+
+       
+        public async Task<int> Create(ProductCreateRequest request)
+        {
+            var product = new Product()
+            {
+                Price = request.Price,
+                OriginalPrice = request.OriginalPrice,
+                Stock = request.Stock,
+                ViewCount = 0,
+                DateCreated = DateTime.Now,
+                ProductTranslations = new List<ProductTranslation>()
+                {
+                    new ProductTranslation()
+                    {
+                        Name = request.Name,
+                        Description = request.Description,
+                        Details = request.Details,
+                        SeoDescription = request.SeoDescription,
+                        SeoAlias = request.SeoAlias,
+                        LanguageId = request.LanguageId
+                    }
+                },
+            };
+            _context.Products.Add(product);
+            return await _context.SaveChangesAsync();
+        }
+
+
+        public async Task<int> Delete(int productId)
+        {
+            var product = await _context.Products.FindAsync(productId);
+            if (product == null) throw new EShopException($"Cannot find a product: {productId}");
+            _context.Products.Remove(product);
+            return await _context.SaveChangesAsync();
+        }
+
+        public async Task<PageResult<ProductViewModel>> GetAllPaging(GetProductPagingRequest request)
+        {
+            var query = from p in _context.Products
+                        join pt in _context.ProductTranslations on p.Id equals pt.ProductId
+                        join pic in _context.ProductInCategories on p.Id equals pic.ProductId
+                        join c in _context.Categories on pic.CategoryId equals c.Id
+                        select new { p, pt,pic };
+            if(!string.IsNullOrEmpty(request.Keyword))
+            {
+                query = query.Where(x => x.pt.Name.Contains(request.Keyword));
+            }
+            if(request.CategoryIds.Count>0)
+            {
+                query = query.Where(p => request.CategoryIds.Contains(p.pic.CategoryId));
+            }
+            int totalRow = await query.CountAsync();
+            var data = await query.Skip((request.PageIndex-1)*request.PageSize)
+                .Take(request.PageSize)
+                .Select(x=>new ProductViewModel()
+                {
+                    Id=x.p.Id,
+                    Name = x.pt.Name,
+                    DateCreated = x.p.DateCreated,
+                    Details = x.pt.Details,
+                    LanguageId = x.pt.LanguageId,
+                    OriginalPrice = x.p.OriginalPrice,
+                    Price = x.p.Price,
+                    SeoAlias = x.pt.SeoAlias,
+                    SeoDescription = x.pt.SeoDescription,
+                    SeoTitle = x.pt.SeoTitle,
+                    Stock = x.p.Stock,
+                    ViewCount = x.p.ViewCount
+                }).ToListAsync();
+
+            var pageResult = new PageResult<ProductViewModel>()
+            {
+                TotalRecord = totalRow,
+                Items = data
+            };
+            return  pageResult;
+        }
+
+        public async Task<int> Update(ProductUpdateRequest request)
+        {
+            var product = await _context.Products.FindAsync(request.Id);
+            var productTralations = await  _context.ProductTranslations.FirstOrDefaultAsync(x => x.ProductId == request.Id&&x.LanguageId==request.LanguageId);
+            if(product==null||productTralations==null) 
+                throw new EShopException($"cannot find a product with id:{request.Id}");
+            productTralations.Name = request.Name;
+            productTralations.SeoAlias = request.SeoAlias;
+            productTralations.SeoDescription = request.SeoDescription;
+            productTralations.SeoTitle = request.SeoTitle;
+            productTralations.Description = request.Description;
+            productTralations.Details = request.Details;
+            return await _context.SaveChangesAsync();
+        }
+
+        public async Task<bool> UpdatePrice(int productId, decimal newPrice)
+        {
+            var product = await _context.Products.FindAsync(productId);
+            if (product == null)
+                throw new EShopException($"cannot find a product with id:{productId}");
+            product.Price = newPrice;
+            return await _context.SaveChangesAsync()>0;
+        }
+
+        public async Task<bool> UpdateStock(int productId, int addedQuantity)
+        {
+            var product = await _context.Products.FindAsync(productId);
+            if (product == null)
+                throw new EShopException($"cannot find a product with id:{productId}");
+            product.Stock += addedQuantity;
+            return await _context.SaveChangesAsync() > 0;
+        }
+    }
+}
